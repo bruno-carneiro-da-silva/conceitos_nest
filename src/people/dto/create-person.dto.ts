970941abf2f7b9b0b770bb0ec3a3d85e18de283d
Bruno / <49591443+bruno-carneiro-da-silva@users.noreply.github.com>
2024-10-03 00:05:58 -0300

    :construction: feat: the person CRUD is done

--- src/people/dto/create-person.dto.ts
+++ src/people/dto/create-person.dto.ts
@@ -1,1 +1,27 @@
-export class CreatePersonDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { Column } from 'typeorm';
+
+export class CreatePersonDto {
+  @IsNotEmpty()
+  @Column({ unique: true })
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(100)
+  name: string;
+
+  @IsStrongPassword()
+  @IsNotEmpty()
+  @MinLength(5)
+  password: string;
+}
--- src/people/people.service.ts
+++ src/people/people.service.ts
@@ -1,26 +1,81 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreatePersonDto } from './dto/create-person.dto';
 import { UpdatePersonDto } from './dto/update-person.dto';
+import { Person } from './entities/person.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PeopleService {
-  create(createPersonDto: CreatePersonDto) {
-    return 'This action adds a new person';
+  constructor(
+    @InjectRepository(Person)
+    private readonly personRepository: Repository<Person>,
+  ) {}
+
+  async create(createPersonDto: CreatePersonDto) {
+    try {
+      const personData = {
+        name: createPersonDto.name,
+        email: createPersonDto.email,
+        passwordHash: createPersonDto.password,
+      };
+
+      const newPerson = this.personRepository.create(personData);
+      await this.personRepository.save(newPerson);
+      return newPerson;
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Email already exists');
+      }
+      throw error;
+    }
   }
 
-  findAll() {
-    return `This action returns all people`;
+  async findAll() {
+    try {
+      const people = await this.personRepository.find({
+        order: {
+          id: 'ASC',
+        },
+      });
+      return people;
+    } catch (error) {
+      throw error;
+    }
   }
 
   findOne(id: number) {
     return `This action returns a #${id} person`;
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  async update(id: number, updatePersonDto: UpdatePersonDto) {
+    const personData = {
+      name: updatePersonDto?.name,
+      passwordHash: updatePersonDto?.password,
+    };
+    const person = await this.personRepository.preload({
+      id,
+      ...personData,
+    });
+
+    if (!person) {
+      throw new NotFoundException('Person not found');
+    }
+
+    return this.personRepository.save(person);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} person`;
+  async remove(id: number) {
+    const person = await this.personRepository.findOneBy({
+      id,
+    });
+    if (!person) {
+      throw new NotFoundException('Person not found');
+    }
+    return this.personRepository.remove(person);
   }
 }
